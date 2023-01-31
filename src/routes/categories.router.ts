99416e7da557321445292db46e241ff526bda02b
Abhishek <abhishek.yps@gmail.com>
2023-01-31 22:32:27 +0530

    ✨ Create Bulk categories.

--- src/routes/categories.router.ts
+++ src/routes/categories.router.ts
@@ -1,18 +1,29 @@
-import { createCategory, getAllCategories } from 'controllers/mart/categories';
+import {
+    createCategory,
+    getAllCategories,
+    categoryBulkCreate,
+} from 'controllers/mart/categories';
 import { createCategoryValidationSchema } from 'controllers/mart/categories/validation';
 import { Router } from 'express';
+import multer from 'multer';
 import { BaseRoute } from 'utils/library/utils';
 import { validate } from 'utils/library/validate';
+import os from 'os';
 
 const router = Router();
+const upload = multer({ dest: os.tmpdir() });
 const bRoute = new BaseRoute('categories');
 const create = bRoute.getRoute('/create');
+const bulCreate = bRoute.getRoute('/bulkCreate');
 const getAll = bRoute.getRoute('/');
 
 console.log({ create });
 
 //Create CATEGORY
 router.post(create, createCategoryValidationSchema, validate, createCategory);
+
+//BULK CREATE CATEGORIES
+router.post(bulCreate, upload.single('csvFile'), categoryBulkCreate);
 
 //GET ALL CATEGORIES
 router.get(getAll, getAllCategories);
